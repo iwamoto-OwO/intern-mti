5d056e174e5b0846ec26f56346918207f2c1427f
Conan0706 <conan.lit.13@gmail.com>
2023-09-13 02:07:38 +0000

    change_baseUrl

--- src/assets/config.js
+++ src/assets/config.js
@@ -1,2 +1,2 @@
 // 自分のエンドポイントに置き換えてください
-export const baseUrl = 'https://2i9it758v9.execute-api.ap-northeast-1.amazonaws.com';
+export const baseUrl = 'https://8bm64mqlw5.execute-api.ap-northeast-1.amazonaws.com';
